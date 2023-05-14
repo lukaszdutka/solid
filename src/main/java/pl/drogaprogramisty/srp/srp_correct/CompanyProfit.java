72bae26e939ddd6c151706c20ed0c89bb08e8b8d
lukaszdutka <lukaszdutka2@gmail.com>
2023-05-14 18:08:28 +0200

    EmployeeSalaryCalculator extracted

--- src/main/java/pl/drogaprogramisty/srp/srp_correct/CompanyProfit.java
+++ src/main/java/pl/drogaprogramisty/srp/srp_correct/CompanyProfit.java
@@ -1,4 +1,4 @@
 package pl.drogaprogramisty.srp.srp_correct;
 
-public record CompanyProfit(int income, int expenses, int profit) {
+public record CompanyProfit(double income, double expenses, double profit) {
 }
